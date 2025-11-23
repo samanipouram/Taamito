ab7beec8df1fd996e1d79fa1f84285d0cbb35ede
samanipouram <mohamadsamanipour@gmail.com>
2025-11-24 02:22:36 +0330

    remove route and resource from product api

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::name('products')->prefix('products')->group(function () {
-    Route::get('/get', [\App\Http\Controllers\ProductController::class, 'getProducts']);
     Route::get('/index', [\App\Http\Controllers\ProductController::class, 'index']);
 });
